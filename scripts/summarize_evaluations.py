c7bc927b67cde5ca2193fab82f37d13f47d36a2c
Henri C. Schmidt <packerfan73@gmail.com>
2025-06-05 10:04:19 -0400

    Evaluate topology search.

--- scripts/summarize_evaluations.py
+++ scripts/summarize_evaluations.py
@@ -3,9 +3,44 @@ import os
 import json
 import re
 import treeswift
+import metrics
+
+from multiprocessing import Pool
 from tqdm import tqdm
 
 import pandas as pd
+
+_FILENAME_RE = re.compile(
+    r"""
+    ^k(?P<characters>\d+)
+    _s(?P<silencing_rate>\d+)
+    _sub(?P<cells>\d+)
+    _r(?P<replicate>\d+)
+    _r(?P<gaussian_distance>[0-9.]+)
+    _p(?P<dropout_rate>[0-9.]+)
+    \.(?P<datatype>.+)$
+    """,
+    re.VERBOSE,
+)
+
+def parse_directory_name(fname):
+    """
+    Extract experiment parameters from a filename.
+    """
+
+    m = _FILENAME_RE.match(fname)
+    if m is None:
+        raise ValueError(f"Filename '{fname}' does not match expected pattern")
+
+    groups = m.groupdict()
+
+    groups["characters"]         = int(groups["characters"])
+    groups["silencing_rate"]     = int(groups["silencing_rate"])
+    groups["cells"]              = int(groups["cells"])
+    groups["gaussian_distance"]  = float(groups["gaussian_distance"])
+    groups["dropout_rate"]       = float(groups["dropout_rate"])
+
+    return groups
 
 def parse_timing_txt(path):
     with open(path) as f:
@@ -25,118 +60,62 @@ def read_branch_lengths(newick):
         node_to_branch_lengths[v.get_label()] = v.get_edge_length()
     return node_to_branch_lengths
 
-def parse_fast_laml_results(subdir):
-    res_path = os.path.join(subdir, 'fast_laml_results.json')
+def parse_fast_laml_results(true_newick_file, starting_tree_newick, subdir):
+    res_path = os.path.join(subdir, 'fastlaml_results.json')
     row = {}
 
     with open(res_path, 'r') as f:
         res = json.load(f)
-        row['nllh'] = res['nllh']
-        row['runtime'] = res['runtime']
+        row['best_log_likelihood'] = res['best_log_likelihood']
+        row['runtime'] = res['runtime_ms']
         row['est_nu'] = res['nu']
         row['est_phi'] = res['phi']
-        row['compile_time'] = res['compile_time']
-        if 'em_iterations' in res:
-            row['em_iterations'] = res['em_iterations']
 
     timing_file = os.path.join(subdir, 'timing.txt')
     if os.path.exists(timing_file):
         res = parse_timing_txt(timing_file)
         row = row | res
 
-    true_newick_file      = os.path.join(subdir, 'simulated_tree.newick')
-    estimated_newick_file = os.path.join(subdir, 'fast_laml_tree.newick')
-    
-    if os.path.exists(true_newick_file) and os.path.exists(estimated_newick_file):
-        true_bls = read_branch_lengths(true_newick_file)
-        est_bls  = read_branch_lengths(estimated_newick_file)
+    estimated_newick_file = os.path.join(subdir, 'fastlaml_ultrametric_tree.nwk')
+    rfs = metrics.compute_rfs(true_newick_file, [estimated_newick_file, starting_tree_newick])
+    bldistances = metrics.compute_branch_length_distances(true_newick_file, [estimated_newick_file, starting_tree_newick])
 
-        l1_bl_error = 0.0
-        l2_bl_error = 0.0
-        for node, bl in true_bls.items():
-            l1_bl_error += abs(bl - est_bls[node])
-            l2_bl_error += (bl - est_bls[node]) ** 2
-
-        row['l1_branch_length_error'] = l1_bl_error
-        row['l2_branch_length_error'] = l2_bl_error
+    row['rf_distance'] = rfs[0]['rf_distance']
+    row['normalized_rf_distance'] = rfs[0]['normalized_rf_distance']
+    row['branch_length_error'] = bldistances[0]['distance']
+    row['initial_rf_distance'] = rfs[1]['rf_distance']
+    row['initial_normalized_rf_distance'] = rfs[1]['normalized_rf_distance']
 
     return row
-
-def parse_laml_results(subdir):
-    row = {}
-
-    with open(os.path.join(subdir, 'LAML_output.log'), 'r') as f:
-        lines = f.readlines()
-        runtime = float(lines[-1].split()[-1])
-        row['runtime'] = runtime
-
-    with open(os.path.join(subdir, 'LAML_output_params.txt'), 'r') as f:
-        lines = f.readlines()
-        row['est_phi'] = float(lines[0].split()[-1])
-        row['est_nu'] = float(lines[1].split()[-1])
-        row['nllh'] = eval(lines[2].split(':')[-1].strip())
-        row['status'] = row['nllh'][1]
-        row['nllh'] = row['nllh'][0]
-
-    timing_file = os.path.join(subdir, 'timing.txt')
-    if os.path.exists(timing_file):
-        res = parse_timing_txt(timing_file)
-        row = row | res
-
-    true_newick_file      = os.path.join(subdir, 'simulated_tree.newick')
-    estimated_newick_file = os.path.join(subdir, 'LAML_output_trees.nwk')
-    
-    if os.path.exists(true_newick_file) and os.path.exists(estimated_newick_file):
-        true_bls = read_branch_lengths(true_newick_file)
-        est_bls  = read_branch_lengths(estimated_newick_file)
-
-        l1_bl_error = 0.0
-        l2_bl_error = 0.0
-        for node, bl in true_bls.items():
-            l1_bl_error += abs(bl - est_bls[node])
-            l2_bl_error += (bl - est_bls[node]) ** 2
-
-        row['l1_branch_length_error'] = l1_bl_error
-        row['l2_branch_length_error'] = l2_bl_error
-
-    return row
-
-def evaluate_algo(algo, algo_res_path):
-    rows = []
-
-    for subdir in tqdm(os.listdir(algo_res_path)):
-        num_chars, alphabet_size, seed_prior, num_cells, seq_prior = re.match(r'k(\d+)M(\d+)p(\d+)_\w+_sub(\d+)_r(\d+)', subdir).groups()
-        row = {
-            'num_chars': num_chars, 
-            'alphabet_size': alphabet_size, 
-            'seed_prior': seed_prior, 
-            'num_cells': num_cells, 
-            'seq_prior': seq_prior,
-            'algorithm': algo
-        }
-
-        if 'fast' in algo:
-            row.update(parse_fast_laml_results(os.path.join(algo_res_path, subdir)))
-        else:
-            row.update(parse_laml_results(os.path.join(algo_res_path, subdir)))
-
-        rows.append(row)
-
-    return rows
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Summarize evaluations')
-    parser.add_argument('directory', type=str, help='Directory containing evaluations')
-    args = parser.parse_args()
+    simulations_dir = "/n/fs/ragr-research/projects/laml-pro/sim_data/set_3/input/"
 
-    dataframe_rows = []
-    for directory in os.listdir(args.directory):
-        algo_res_path = os.path.join(args.directory, directory)
-        if not os.path.isdir(algo_res_path):
-            continue
+    jobs = []
+    for directory in os.listdir("nextflow_results/fast-laml"):
+        data = parse_directory_name(directory)
+        instance_id = f"k{data['characters']}_s{data['silencing_rate']}_sub{data['cells']}_r{data['replicate']}"
+        simulation_dir = os.path.join(simulations_dir, instance_id)
+        for subdirectory in os.listdir(os.path.join("nextflow_results/fast-laml", directory)):
+            true_tree = os.path.join(simulation_dir, "tree.nwk")
+            starting_tree = f"nextflow_results/starting_trees/{instance_id}_r{data['gaussian_distance']}_p{data['dropout_rate']}"
+            starting_tree = os.path.join(starting_tree, subdirectory)
+            jobs.append({
+                "data": data,
+                "true_tree": true_tree, 
+                "starting_tree": starting_tree, 
+                "subdirectory": os.path.join("nextflow_results/fast-laml", directory, subdirectory),
+                "starting_tree_type": subdirectory
+            })
 
-        rows = evaluate_algo(directory, algo_res_path)
-        dataframe_rows.extend(rows)
+    def evaluate_job(job):
+        row = parse_fast_laml_results(job['true_tree'], job['starting_tree'], job['subdirectory'])
+        row = row | job["data"]
+        row['starting_tree'] = job["starting_tree_type"]
+        return row
 
-    df = pd.DataFrame(dataframe_rows)
-    df.to_csv('evaluations.csv', index=False)
+    with Pool(64) as p:
+        rows = p.map(evaluate_job, jobs)
+
+    df = pd.DataFrame(rows)
+    df.to_csv("observation_model_results.csv")
